6fafe710824091134d68acc3b4c764e2a505f638
myo-i <Imiyo0521@gmail.com>
2021-01-10 16:48:27 +0900

    検索フォーム　routing, view実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: 'ideas#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :ideas do
-    resources :tweets
+    resources :tweets, only: [:index, :new, :create, :show, :destroy] do 
+      collection do
+        get 'search'
+      end
+    end
   end
 end
